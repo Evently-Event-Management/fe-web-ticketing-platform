5473b8d9332682d13163d91854b200cc5dc91778
Dasun Piyumal <w.piyumal2319@gmail.com>
2025-08-17 21:27:26 +0530

    Add seating information and layout components; update next.config.ts for S3 hostname

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,10 @@ const nextConfig: NextConfig = {
             {
                 protocol: 'http',
                 hostname: 'event-seating-localstack'
+            },
+            {
+                protocol: 'https',
+                hostname: 'ticketly-storage.s3.amazonaws.com'
             }
         ],
     },
