d01c766adc169465cdca71ea676d707f26f48b44
Dasun Piyumal <w.piyumal2319@gmail.com>
2025-10-10 03:11:46 +0530

    feat: integrate AI-powered event overview generation and enhance image handling in event settings

--- src/app/(home-app)/events/[event_id]/layout.tsx
+++ src/app/(home-app)/events/[event_id]/layout.tsx
@@ -1,15 +1,16 @@
-import React, {Suspense} from 'react';
-import {EventHero, ReviewEventHeroSkeleton} from "@/app/(home-app)/events/[event_id]/_components/EventHero";
-import {Separator} from "@/components/ui/separator";
-import {getEventSummery, getEventTotalViews} from "@/lib/actions/public/server/eventActions";
-import {EventOverview} from "@/app/manage/_components/review/EventOverview";
+import React, { Suspense } from 'react';
+import { EventHero, ReviewEventHeroSkeleton } from "@/app/(home-app)/events/[event_id]/_components/EventHero";
+import { Separator } from "@/components/ui/separator";
+import { getEventSummery, getEventTotalViews } from "@/lib/actions/public/server/eventActions";
+import { EventOverview } from "@/app/manage/_components/review/EventOverview";
+import { EventTracker } from './_components/EventTracker';
 
 
-export default async function Layout({params, children}: {
+export default async function Layout({ params, children }: {
     params: Promise<{ event_id: string }>
     children: React.ReactNode;
 }) {
-    const {event_id} = await params;
+    const { event_id } = await params;
     const eventSummery = await getEventSummery(event_id);
     const viewsData = await getEventTotalViews(event_id);
 
@@ -17,13 +18,18 @@ export default async function Layout({params, children}: {
         <div>
             <div className="min-h-screen p-4 sm:p-6 md:p-8">
                 <div className="max-w-7xl mx-auto space-y-8">
-                    <Suspense fallback={<ReviewEventHeroSkeleton/>}>
+                    <Suspense fallback={<ReviewEventHeroSkeleton />}>
+                        <EventTracker event={{
+                            title: eventSummery.title,
+                            id: eventSummery.id,
+                            organization_id: eventSummery.organization?.id || '',
+                        }} />
                         <EventHero event={eventSummery}
-                                   viewCount={viewsData.success ? viewsData.viewCount : undefined}/>
-                        <Separator className={'border-3 my-2'}/>
-                        <EventOverview overview={eventSummery.overview}/>
+                            viewCount={viewsData.success ? viewsData.viewCount : undefined} />
+                        <Separator className={'border-3 my-2'} />
+                        <EventOverview overview={eventSummery.overview} />
                     </Suspense>
-                    <Separator className={'border-3 my-2'}/>
+                    <Separator className={'border-3 my-2'} />
                     {children}
                 </div>
             </div>
