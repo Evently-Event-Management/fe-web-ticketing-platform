c701b0b3cc018ef7721dc9950e107da26662a89f
Dasun Piyumal <w.piyumal2319@gmail.com>
2025-10-11 17:22:15 +0530

    test init

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from 'cypress';
+const { devServer } = require('@cypress/webpack-dev-server');
+const webpackConfig = require('./webpack.config.js');
 
 export default defineConfig({
   e2e: {
@@ -9,6 +11,18 @@ export default defineConfig({
     // Handle cross-origin testing (for auth.ticketly.com)
     chromeWebSecurity: false,
     testIsolation: false
+  },
+  component: {
+    devServer(devServerConfig) {
+      return devServer({
+        ...devServerConfig,
+        framework: 'react',
+        webpackConfig,
+      });
+    },
+    specPattern: 'cypress/component/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: './cypress/support/component.ts',
+    indexHtmlFile: './cypress/support/component-index.html'
   },
   viewportWidth: 1280,
   viewportHeight: 720,
